1a34d784e2c0af549a420a1a015db96d2ef802e3
Ivan <ivanmalikoval@gmail.com>
2024-03-29 15:19:02 +0300

    Post app comp_API

---
+++ vue-comp/src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<script setup>
+import PostItem from '@/components/PostItem.vue'
+import MyWrapper from '@/components/MyWrapper.vue'
+
+const posts = [
+  {
+    id: 1,
+    title: 'A great book!',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quasi praesentium, assumenda molestias est deleniti.',
+    author: 'Joe Mama',
+    created_at: '11/06/2023',
+    is_saved: false
+  },
+  {
+    id: 2,
+    title: 'Cool book!',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quasi praesentium, assumenda molestias est deleniti.',
+    author: 'Joe Mama',
+    created_at: '23/06/2023',
+    is_saved: false
+  },
+  {
+    id: 3,
+    title: 'Im bored!',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quasi praesentium, assumenda molestias est deleniti.',
+    author: 'Joe Mama',
+    created_at: '30/06/2023',
+    is_saved: false
+  },
+  {
+    id: 4,
+    title: 'Coding FF',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quasi praesentium, assumenda molestias est deleniti.',
+    author: 'Joe Mama',
+    created_at: '05/07/2023',
+    is_saved: false
+  }
+]
+</script>
+<template>
+  <div v-for="(post) in posts" :key="post.id">
+    <MyWrapper>
+      <PostItem :post="post" />
+    </MyWrapper>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue-comp/src/views/PostCreate.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div>
+        Create a new post
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
